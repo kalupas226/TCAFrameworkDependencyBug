47f943cfaf9121e2604b6cdf24b1f9d5a5d2b6eb
kalupas226 <kalupas226@gmail.com>
2022-12-28 14:23:14 +0900

    Add bug project

---
+++ TCAFrameworkDependencyBug/TCAFrameworkDependencyBug/TCAFrameworkDependencyBugApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAFrameworkDependencyBugApp.swift
+//  TCAFrameworkDependencyBug
+//
+//  Created by kalupas226 on 2022/12/28.
+//
+
+import SwiftUI
+
+@main
+struct TCAFrameworkDependencyBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
